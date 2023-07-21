9c80a6b5f61bd0c628bcab9d20fee563a18e7adc
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-07-21 10:42:41 -0700

    Initiate the repository

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Stock Info</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
+</head>
+
+<body>
+  <header>
+  </header>
+  <main>
+  </main>
+  <footer>
+    <div class="footer">
+        <div class="allrights">© Seisuke Yamada All rights reserved.</div>
+    </div>
+  </footer>
+</body>
+
+
+
+</html>
